382624ee9c7cf2415f0ae880b5334b92f47e00a2
Omar Guenoun <guenouno@beluga3.int.ets1.calculquebec.ca>
2025-07-21 16:41:38 -0400

    First figure , still need to make it cleaner

--- AUM_comparison.r
+++ AUM_comparison.r
@@ -253,6 +253,32 @@ if(file.exists(cache.RData)){
 score_dt <- mlr3resampling::score(bench.result, measure_list)
 score_out <- score_dt[, .(
   task_id, test.subset, train.subsets, test.fold, algorithm, auc_micro,auc_macro)]
-history_dt <- score_dt[
-  , learner[[1]]$model
-  , by=.(task_id, test.subset, train.subsets, test.fold, algorithm)]
+summary_dt <- score_out[, .(
+  mean_auc_micro = mean(auc_micro),
+  sd_auc_micro = sd(auc_micro),
+  mean_auc_macro = mean(auc_macro),
+  sd_auc_macro = sd(auc_macro)
+), by = .(test.subset, train.subsets, algorithm)]
+long_dt <- melt(summary_dt,
+                measure = patterns(mean = "^mean_auc", sd = "^sd_auc"),
+                variable.name = "metric",
+                value.name = c("mean", "sd")
+)
+long_dt[, metric := factor(metric, labels = c("auc_micro", "auc_macro"))]
+long_dt[, test.subset := paste0("test = ", test.subset)]
+
+
+ggplot(long_dt, aes(x = mean, y = algorithm, color = metric)) +
+  geom_point(position = position_dodge(width = 0.5), size = 2) +
+  geom_errorbarh(
+    aes(xmin = mean - sd, xmax = mean + sd),
+    position = position_dodge(width = 0.5),
+    height = 0.25
+  ) +
+  facet_grid(test.subset ~ train.subsets) +
+  labs(
+    title = "AUC Mean ± SD by Algorithm",
+    x = "AUC",
+    y = "Algorithm",
+    color = "Metric"
+  )
